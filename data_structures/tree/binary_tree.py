ea74430807cb8986ccedc7ab5c5ab936f6a2d5d6
embejer <emforwork09@gmail.com>
2023-11-24 21:45:00 +0800

    2nd push for binary_tree

---
+++ data_structures/tree/binary_tree.py
@@ -0,0 +1,43 @@
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+class Tree(Generic[T]):
+    def __init__(self, val: int):
+        self.left = None
+        self.val = val
+        self.right = None
+        self.value_list = []
+
+    def append(self, val: int) -> T:
+        if self.val is None:
+            self.val = val
+        else:
+            if val < self.val:
+                if self.left is None:
+                    self.left = Tree(val)
+                else:
+                    self.left.append(val)
+            elif val > self.val:
+                if self.right is None:
+                    self.right = Tree(val)
+                else:
+                    self.right.append(val)
+
+
+    def display(self, tree):
+        if tree is None:
+            return self.value_list
+        else:
+            self.display(tree.left)
+            self.value_list.append(tree.val)
+            self.display(tree.right)
+        return self.value_list
+
+
+if __name__ == '__main__':
+    tree = Tree(0)
+    tree.append(1)
+    tree.append(2)
+    numbers = tree.display(tree)
+    print(numbers)
